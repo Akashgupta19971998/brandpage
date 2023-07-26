c1c5f163dad66442cc434b7bd2facda55858309b
Akash Kumar Gupta <117676075+Akashgupta19971998@users.noreply.github.com>
2023-07-26 21:30:36 +0530

    Add files via upload

---
+++ src/components/Navigation.js
@@ -0,0 +1,19 @@
+const Navigation = () => {
+    return (
+      <nav className="container">
+        <div className="logo">
+          <img src="/images/brand_logo.png" alt="logo" />
+        </div>
+       <div className="akash">
+          <a href="https://www.nike.com/in/w/mens-shoes-nik1zy7ok">Menu</a>
+          <a href="https://www.nike.com/in/w/mens-shoes-nik1zy7ok">Location</a>
+          <a href="https://www.nike.com/in/w/mens-shoes-nik1zy7ok">About</a>
+          <a href="https://www.nike.com/in/w/mens-shoes-nik1zy7ok">Contact</a>
+          </div>
+        <button>login</button>
+      </nav>
+    );
+  };
+  
+  export default Navigation;
+  
